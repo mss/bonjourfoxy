2b6cf597ebd7a6bba96b6acd774c287e174539a4
andrew@tj.id.au <andrew@tj.id.au>
2009-04-03 13:46:36 +1100

    Fixes, release 0.4 * Moved db init to bfnotifier to ensure db is ready for use by other componenets * Updated rdf's to suit 0.4 release

--- content/firstrun.js
+++ content/firstrun.js
@@ -11,81 +11,12 @@ function FirstRunCheck()
     if(curRev!=newRev)   {
         prefs.setIntPref("rev",newRev);
         if (curRev==0)  {
-            databaseInit();
             prefs.setIntPref("hts", Math.round(now.getTime()/1000.0));
-            setTimeout("AddToolbarItem();", 0);
+            setTimeout("AddToolbarItem();", 1);
             setTimeout('toggleSidebar("viewBonjourServices");',2);
         }
         setTimeout("OpenWelcome();", 1);
     }
-}
-
-function databaseInit()   {
-  var storageService = Components.classes["@mozilla.org/storage/service;1"]
-                        .getService(Components.interfaces.mozIStorageService);
-
-  var dbFile = Components.classes["@mozilla.org/file/directory_service;1"]
-                    .getService(Components.interfaces.nsIProperties)
-                    .get("ProfD", Components.interfaces.nsIFile);
-  dbFile.append("bonjourfoxy.sqlite");
-  var DBConn = storageService.openDatabase(dbFile);
-  var dbQueries = ['                                    \
-CREATE TABLE Services (                                 \
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,    \
-    "regtype" TEXT,                                     \
-    "scheme" TEXT,                                      \
-    "label" TEXT,                                       \
-    UNIQUE("regtype")                                   \
-);                                                      \
-    ','                                                 \
-CREATE TABLE ServiceSubtypes (                          \
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,    \
-    "s_id" INTEGER NOT NULL,                            \
-    "subtype" TEXT,                                     \
-    "label" TEXT,                                       \
-    UNIQUE("id", "s_id", "subtype", "label")            \
-);                                                      \
-    ','                                                 \
-CREATE TABLE ServicesLastInsert (                       \
-    "row_id" INTEGER                                    \
-);                                                      \
-    ','                                                 \
-INSERT INTO ServicesLastInsert values (-1);             \
-    ','                                                 \
-CREATE TRIGGER SST_s_id_ENFORCE                         \
-BEFORE INSERT ON ServiceSubtypes                        \
-FOR EACH ROW BEGIN                                      \
-    SELECT RAISE(ROLLBACK, "ServiceSubtypes(s_id) must match one of Services(id)")  \
-    WHERE (SELECT id FROM Services WHERE id = NEW.s_id) IS NULL;    \
-END;                                                    \
-    ','                                                 \
-CREATE TRIGGER S_SST_CLEAN                              \
-BEFORE DELETE ON Services                               \
-FOR EACH ROW BEGIN                                      \
-    DELETE from ServiceSubtypes WHERE s_id = OLD.id;    \
-END;                                                    \
-    ','                                                 \
-INSERT INTO Services (regtype, scheme, label)           \
-VALUES (                                                \
-    "_http._tcp.",                                      \
-    "http://${iftxtvalue:u:${txtvalue:u}}${iftxtvalue:u:${iftxtvalue:p::${txtvalue:p}}}${iftxtvalue:u:@}${srv:hostname}:${srv:port}${ifnxtcharsne:/:/}${txtvalue:path}",    \
-    "Websites"                                          \
-    );                                                  \
-    ','                                                 \
-INSERT INTO ServiceSubtypes (s_id, subtype, label)      \
-VALUES (1, "_wiki", "Wikis");                           \
-    ','                                                 \
-INSERT INTO ServiceSubtypes (s_id, subtype, label)      \
-VALUES (1, "_printer", "Printers");                     \
-    '];
-    for (i=0;i<dbQueries.length;i++)    {
-        try { DBConn.executeSimpleSQL(dbQueries[i]); }
-        catch (e)   {
-            dump (['Error executing dbQueries['+i+']',DBConn.lastError,DBConn.lastErrorString,"\n"].join(' '));
-            break;
-        }
-    }
-    
 }
 
 function AddToolbarItem()
